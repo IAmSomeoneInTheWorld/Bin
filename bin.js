a58c68ab12b890b5b6e52f7210dfb1799c966504
Madhavendra Dubey <gmind2895@gmail.com>
2022-07-25 19:11:12 +0530

    fixed some mistakes

--- bin.js
+++ bin.js
@@ -2,6 +2,3 @@ module.exports = {
     string : require("./lib/BtoS/b2sTranslater"),
     binary : require("./lib/StoB/translate")
 };
-
-const b=require("./lib/StoB/translate")
-console.log(b('hello'))
